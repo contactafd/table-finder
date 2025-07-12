89e099a53160322ed00d67d20e2d7f67778c22b5
Francisco Aguirre <fjan05@icloud.com>
2025-07-12 01:25:32 -0400

    Rename jpg lowercase  and other tweaks

--- make_qr.py
+++ make_qr.py
@@ -1,5 +1,5 @@
 import qrcode
 
-URL = "http://127.0.0.1:5000"          # change after you deploy to the cloud
+URL = "https://table-finder-9xkv.onrender.com"          # change after you deploy to the cloud
 qrcode.make(URL).save("table_qr.png")
 print("✓ QR code saved to table_qr.png")
